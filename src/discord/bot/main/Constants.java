5c08d1a4db3221745eb529d104c2af68f7488f9e
shnewton <45050434+shnewton@users.noreply.github.com>
2018-12-21 23:00:32 -0500

    Update Constants.java

--- src/discord/bot/main/Constants.java
+++ src/discord/bot/main/Constants.java
@@ -1,6 +1,6 @@
 package discord.bot.main;
 
 public class Constants {
-    public static final String TOKEN = "MzkzNDkxMDQxMzc2MDEwMjQw.DusP3Q.FyUTKYOj_kOelxpXhdb1KCE3TLA";
+    public static final String TOKEN = "";
     public static final String PREFIX = "!";
 }
